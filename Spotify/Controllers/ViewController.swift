2bdbb82f06eae8e31e1c1b752c56e299f3e38a53
Titus Herdiawan <76503729+titusherd@users.noreply.github.com>
2022-06-18 19:27:16 +0700

    Create xcode and remove storyboard + turning background to red

---
+++ Spotify/Controllers/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Spotify
+//
+//  Created by Titus Herdiawan on 18/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        title = "Home"
+        view.backgroundColor = .red
+    }
+    
+    
+
+
+}
+
